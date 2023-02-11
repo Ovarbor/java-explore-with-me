1a8f1d6cbb9ed4b39d5b5adb5d33b25ef9d98a90
Ovabor <ovabor1991@gmail.com>
2023-02-11 19:37:17 +0300

    status 201

--- statistic/stat-service/src/main/java/ru/practicum/statservice/controller/StatController.java
+++ statistic/stat-service/src/main/java/ru/practicum/statservice/controller/StatController.java
@@ -2,6 +2,7 @@ package ru.practicum.statservice.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.statdto.dto.HitDto;
@@ -21,19 +22,19 @@ public class StatController {
     private final StatService service;
 
     @PostMapping("/hit")
-    public HitDto saveStat(@RequestBody @Valid Hit hit) {
+    public ResponseEntity <HitDto> saveStat(@RequestBody @Valid Hit hit) {
         log.info("POST: /hit");
-        return service.addHits(hit);
+        return ResponseEntity.status(201).body(service.addHits(hit));
     }
 
     @GetMapping("/stats")
-    public List<ViewStats> getStat(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public ResponseEntity <List<ViewStats>> getStat(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
                                          LocalDateTime start,
                                      @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
                                          LocalDateTime end,
                                      @RequestParam List<String> uris,
                                      @RequestParam(defaultValue = "false") boolean unique) {
         log.info("GET: /stats");
-        return service.getStats(start, end, uris, unique);
+        return ResponseEntity.ok().body(service.getStats(start, end, uris, unique));
     }
 }
